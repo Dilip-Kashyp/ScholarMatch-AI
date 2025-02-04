1b51bb5d4cf444f990ed0c99df2cd32b10ac6cc5
Dilip Kumar <dilipkumar1.edu@gmail.com>
2025-02-04 13:45:14 +0530

    enha: about page

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -1,5 +1,5 @@
 import { Container, Paper, Stack, Typography } from "@/components";
-import { ABOUT_PAGE_CONFIG } from "@/constants";
+import { ABOUT_PAGE_CONFIG, SEARCH_QUERIES } from "@/constants";
 
 function AboutPage() {
   const {
@@ -7,11 +7,12 @@ function AboutPage() {
     DESCRIPTION_CONFIG,
     HOW_IT_WORKS_HEADER_CONFIG,
     HOW_IT_WORKS_DESCRIPTION_CONFIG,
-    TECHSTACK_HEADER_CONFIG,
-    TECHSTACK_DESCRIPTION_CONFIG,
+    SEARCH_QUERY_CONFIG,
+    SEARCH_QUERIES,
   } = ABOUT_PAGE_CONFIG;
   return (
     <>
+
       <Container
         containerProps={{
           className: "min-h-screen flex flex-col  items-center gap-24 py-12",
@@ -19,18 +20,27 @@ function AboutPage() {
       >
         <Paper
           paperProps={{
-            className: "p-8 min-w-[80%] max-w-[100%] flex flex-col gap-6",
+            className: "p-4 md:p-8 min-w-[80%] max-w-[100%] flex flex-col gap-6",
           }}
         >
           <Stack
 
-            stackProps={{ direction: "column", gap: 3, alignItems: "center" }}
+            stackProps={{ direction: "column", gap: 3,  }}
           >
             <Typography {...HEADER_CONFIG} />
             <Typography {...DESCRIPTION_CONFIG} />
+            <Stack
+              stackProps={{ direction: "column", gap: 3 }}
+            >
+              {SEARCH_QUERIES?.map((query, index) => (
+                <Typography key={index} {...SEARCH_QUERY_CONFIG(query)} />
+              ))}
+            </Stack>
           </Stack>
           <Stack
+
             stackProps={{ direction: "column", gap: 3, alignItems: "center" }}
+
           >
             <Typography {...HOW_IT_WORKS_HEADER_CONFIG} />
             <Typography {...HOW_IT_WORKS_DESCRIPTION_CONFIG} />
--- src/constants/about.js
+++ src/constants/about.js
@@ -1,23 +1,35 @@
 export const ABOUT_PAGE_CONFIG = {
+  SEARCH_QUERY_CONFIG: (query) => {
+    return {
+      typographyProps: {
+        variant: "h6",
+        children: `* ${query}`,
+        sx: {
+          color: "gray",
+        },
+
+
+      },
+    };
+  },
+
+
+
   HEADER_CONFIG: {
     typographyProps: {
       variant: "h5",
-      children: "How to use our platform",
       align: "center",
+      children: "How to use our platform",
     },
   },
   DESCRIPTION_CONFIG: {
     typographyProps: {
       variant: "h6",
-      align: "center",
       children:
-        "Go to the scholarship page and search for scholarships using the search bar. Search query may be like 'scholarship for SC student in Chennai' or 'scholarship for amount greater than 80000' or 'scholarship for student disabled' or 'scholarship for girl student'",
+        "Go to the scholarship page and search for scholarships using the search bar. Search query may be like  ",
       sx: {
         color: "gray",
       },
-
-
-
     },
   },
   HOW_IT_WORKS_HEADER_CONFIG: {
@@ -30,31 +42,36 @@ export const ABOUT_PAGE_CONFIG = {
   HOW_IT_WORKS_DESCRIPTION_CONFIG: {
     typographyProps: {
       variant: "h6",
-      align: "center",
+      // align: { xs: "center", md: "left" },
       children:
         "Integrating modern technologies, this application securely connects to a PostgreSQL database to fetch scholarship records and uses an AI service to convert details into semantic vectors while extracting query parameters. It leverages Gemini functions: one extracts search details (like type, location, and amount) from a user’s query using the Gemini-pro model, and another converts text into vectors with the embedding-001 model. These vectors are stored in a Pinecone index and combined with traditional filters, ensuring students find precise, tailored scholarship recommendations.",
       sx: {
-
         color: "gray",
       },
     },
   },
-//   TECHSTACK_HEADER_CONFIG: {
-//     typographyProps: {
-//       variant: "h5",
-//       children: "Tech Stack",
-//       align: "center",
-//     },
-//   },
-//   TECHSTACK_DESCRIPTION_CONFIG: {
-//     typographyProps: {
-//       variant: "h6",
-//       children:
-//         "Our platform uses AI-powered search to match students with scholarships that fit their profile.",
-//       align: "center",
-//       sx: {
-//         color: "gray",
-//       },
-//     },
-//   },
+  //   TECHSTACK_HEADER_CONFIG: {
+  //     typographyProps: {
+  //       variant: "h5",
+  //       children: "Tech Stack",
+  //       align: "center",
+  //     },
+  //   },
+  //   TECHSTACK_DESCRIPTION_CONFIG: {
+  //     typographyProps: {
+  //       variant: "h6",
+  //       children:
+  //         "Our platform uses AI-powered search to match students with scholarships that fit their profile.",
+  //       align: "center",
+  //       sx: {
+  //         color: "gray",
+  //       },
+  //     },
+  //   },
+  SEARCH_QUERIES: [
+    "scholarship for SC student in Chennai",
+    "scholarship for amount greater than 80000",
+    "scholarship for student disabled",
+    "scholarship for girl student",
+  ],
 };
